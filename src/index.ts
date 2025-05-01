ef06ff0307f1b82c6f44d43c60a171fa94007b57
Coby <coby@o1labs.org>
2025-05-01 16:18:40 -0400

    add NTS exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
-export * from './FungibleToken.js';
-export * from './FungibleTokenAdmin.js';
+export {
+  MintConfig,
+  MintParams,
+  BurnConfig,
+  BurnParams,
+  MintDynamicProofConfig,
+  BurnDynamicProofConfig,
+  TransferDynamicProofConfig,
+  UpdatesDynamicProofConfig,
+  DynamicProofConfig,
+} from './configs.js';
+
+export { 
+    FungibleTokenErrors,
+    FungibleToken,
+    SetAdminEvent,
+    MintEvent,
+    BurnEvent,
+    BalanceChangeEvent,
+    VKeyMerkleMap,
+} from './NewTokenStandard.js'
+
+export { generateDummyDynamicProof, SideloadedProof } from './side-loaded/program.eg.js';
